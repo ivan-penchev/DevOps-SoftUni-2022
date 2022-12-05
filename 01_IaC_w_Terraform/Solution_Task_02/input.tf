1b9cd73b8a288a1a400b88810bd63591ac0cf318
Ivan Penchev <30929349+ivan-penchev@users.noreply.github.com>
2022-12-05 21:18:42 +0100

    chore: remove something

--- 01_IaC_w_Terraform/Solution_Task_02/input.tf
+++ 01_IaC_w_Terraform/Solution_Task_02/input.tf
@@ -22,8 +22,3 @@ variable "client_secret" {
   default = ""
 }
 
-
-#   subscription_id = "8b600cf5-d87c-4bb5-b69f-1bacd64f2b97"
-#   tenant_id       = "08a5a19c-7bb6-4241-9785-f035b360699a"
-#   client_id       = "fdc62387-d1d0-49e9-b9e7-80a9c4ffa347"
-#   client_secret   = "a_H8Q~U4_iQLLaQfRuwtHLhMuv9UKcZuefNgYbcE"
